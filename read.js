c9cbdddc44f77fbd537cfe260f61b9b835273079
James Burgess <jamesmillerburgess@gmail.com>
2018-02-07 11:18:03 +0100

    sort

--- read.js
+++ read.js
@@ -4,19 +4,16 @@ var fs = require('fs'),
 var parser = new xml2js.Parser();
 
 fs.readFile('./tmp/10.xml', function (err, data) {
-  console.log(err);
-  console.log(data);
   parser.parseString(data, function (err, result) {
-    console.log(err);
-    console.log(result);
     let tests = [];
     result.testsuites.testsuite
       .map(testsuite => testsuite.testcase)
       .forEach(testcase =>
         testcase.forEach(({ $: { name, time } }) =>
-          tests.push({ name, time })
+          tests.push({ name, time: +time })
         )
       );
+    tests.sort((a, b) => a.time - b.time)
     console.log(tests);
   });
 });
