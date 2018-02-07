64bbd0eb5fd9c5698c0fdfdb6d552885838f0e8a
James Burgess <jamesmillerburgess@gmail.com>
2018-02-07 20:53:12 +0100

    log previous

--- read.js
+++ read.js
@@ -3,10 +3,16 @@ var fs = require('fs'),
 
 var parser = new xml2js.Parser();
 
+// Load previous test balance
+const previous = JSON.parse(fs.readFileSync(`./test-groups/${i}.xml`));
+console.log(previous);
+
+
+
 // Load most recent junit results
 let tests = [];
 for (let i = 0; i < 4; i++) {
-  const data = fs.readFileSync(`./tmp/${i}.xml`);
+  const data = fs.readFileSync(`./results/${i}.xml`);
 
   parser.parseString(data, function (err, result) {
     result.testsuites.testsuite
@@ -24,12 +30,9 @@ tests.sort((a, b) => b.time - a.time)
 console.log(tests);
 
 // Split into groups
-const groups = [[tests[0]], [], [], []];
-const totals = [tests[0].time, 0, 0, 0];
-// const getMax = () => totals.reduce((max, total, i) => total > totals[max] ? i : max, 0);
+const groups = [[], [], [], []];
+const totals = [0, 0, 0, 0];
 const getMin = () => totals.reduce((max, total, i) => total < totals[max] ? i : max, 0);
-let i = 1;
-let prev = 0;
 
 // Naive but simple allocation
 tests.forEach(test => {
