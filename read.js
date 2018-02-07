8fc12da120adef0394e7447c6a80a4e3e7b4cc4f
James Burgess <jamesmillerburgess@gmail.com>
2018-02-07 09:52:27 +0100

    Initial commit

---
+++ read.js
@@ -0,0 +1,19 @@
+var fs = require('fs'),
+  xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
+
+fs.readFile('./10.xml', function (err, data) {
+  parser.parseString(data, function (err, result) {
+    console.log(result);
+    let tests = [];
+    result.testsuites.testsuite
+      .map(testsuite => testsuite.testcase)
+      .forEach(testcase =>
+        testcase.forEach(({ $: { name, time } }) =>
+          tests.push({ name, time })
+        )
+      );
+    console.log(tests);
+  });
+});
---
+++ write.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+const xml =
+  `<testsuites>
+<testsuite name="shell-tests" tests="1" failures="0" errors="0" time="265.57">
+  <testcase name="meteor shell" time="85.64"/>
+</testsuite>
+<testsuite name="source-maps" tests="1" failures="0" errors="0" time="265.57">
+  <testcase name="source maps from checkout" time="1.26"/>
+</testsuite>
+<testsuite name="static-html" tests="2" failures="0" errors="0" time="265.57">
+  <testcase name="static-html - add static content to head and body" time="53.737"/>
+  <testcase name="static-html - throws error" time="51.069"/>
+</testsuite>
+  <testsuite name="stylus-cross-packages" tests="1" failures="0" errors="0" time="265.57">
+  <testcase name="can import stylus across packages" time="73.86"/>
+</testsuite>
+</testsuites>`;
+
+fs.writeFile("./10.xml", xml, function (err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+}); 
