35dc3f05018b4bc83f99fe9006b8ac015dc4fdd0
James Burgess <jamesmillerburgess@gmail.com>
2018-02-08 01:40:33 +0100

    Clean up read

--- read.js
+++ read.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 
-const args = process.argv.slice(2);
-
-const numGroups = +args[args.indexOf('--num-groups') + 1];
-
 const parser = new xml2js.Parser();
 
-// Load most recent junit results
-let results = [];
+const args = process.argv.slice(2);
+const numGroups = +args[args.indexOf('--num-groups') + 1];
+const runningAvgLenth = +args[args.indexOf('--running-avg-length') + 1];
+
+// Load the junit results from the various groups on this build
+let currentBuildResults = [];
 for (let i = 0; i < numGroups; i++) {
   const data = fs.readFileSync(`./results/${i}.xml`);
   parser.parseString(data, function (err, result) {
@@ -16,22 +16,24 @@ for (let i = 0; i < numGroups; i++) {
       .map(testsuite => testsuite.testcase)
       .forEach(testcase =>
         testcase.forEach(({ $: { name, time } }) => {
-          results.push({ name, time: +time })
+          currentBuildResults.push({ name, time: +time })
         })
       );
   });
 }
 
-// Load previous test balance
-let previousResults;
+// Try to load previous test balance
+let previousBuildResults = [];
 try {
-  previousResults = JSON.parse(fs.readFileSync(`./test-groups/data.json`));
+  previousBuildResults = JSON.parse(fs.readFileSync(`./test-groups/data.json`));
 } catch (err) {
   console.log(err);
 }
 
-// Add new results and limit the record to five
-const allBuilds = [results, ...previousResults].slice(0, 5);
+// Add new results and limit the record to the specified number for the running
+// average
+const allBuildResults =
+  [currentBuildResults, ...previousBuildResults].slice(0, runningAvgLenth);
 
 let averageResults = [];
 
@@ -39,12 +41,14 @@ let averageResults = [];
 // next time, so we only calculate the averages for those. This is will ignore
 // any tests that were not run most recently but which were run in previous
 // builds
-results.forEach(result => {
-  const { name } = result;
+currentBuildResults.forEach(({ name }) => {
   const times = [];
 
-  allBuilds.forEach(build => {
-    const { time } = build.find(test => test.name === name) || {};
+  // Check each build result to see if the test was run
+  allBuildResults.forEach(buildResults => {
+    const { time } = buildResults.find(test => test.name === name) || {};
+
+    // It's possible this particular test wasn't run in this build
     if (time !== undefined) {
       times.push(!isNaN(time) ? time : 0);
     }
@@ -52,35 +56,44 @@ results.forEach(result => {
 
   const averageTime =
     Math.floor(times.reduce((p, x) => p + x, 0) / times.length * 1000) / 1000;
+
   averageResults.push({ name, time: averageTime });
 });
 
-averageResults.sort((a, b) => b.time - a.time);
-
-// Split into groups
-const groups = Array(numGroups);
+// This is a naïve but simple allocation which just take the largest remaining
+// test and puts it in the group with the lowest total
+const groupTestNames = Array(numGroups);
 const totals = Array(numGroups).fill(0);
 const getMin = () =>
   totals.reduce((min, total, i) => total < totals[min] ? i : min, 0);
 
-averageResults.forEach(test => {
+averageResults.sort((a, b) => b.time - a.time);
 
-  // Naïve but simple allocation
+averageResults.forEach(({ name, time }) => {
   const min = getMin();
-  groups[min] ? groups[min].push(test) : groups[min] = [test];
-  totals[min] += test.time;
-  console.log(test);
-  console.log(totals);
+
+  groupTestNames[min]
+    ? groupTestNames[min].push(name)
+    : groupTestNames[min] = [name];
+
+  totals[min] += time;
 });
 
-console.log('Groups:');
-console.log(groups);
-
-groups.forEach((group, i) => {
-  const text = `^${group.map(test => test.name).join('$|^')}$`;
-  fs.writeFileSync(`./test-groups/${i}.txt`, text);
-  console.log(`Group ${i}:`);
-  console.log(text);
+// Create the (long) regular expressions for each group
+groupTestNames.forEach((names, i) => {
+  const testMatcher = `^${names.join('$|^')}$`;
+  fs.writeFileSync(`./test-groups/${i}.txt`, testMatcher);
+  console.log(`Group ${i} Test Names:`);
+  console.log(testMatcher);
 });
 
-fs.writeFileSync("./test-groups/data.json", JSON.stringify(allBuilds)); 
+// Create the (even longer) regular expression to catch any other tests that
+// might get added to the suite or which may have been turned off for the most
+// recent build
+const otherTestsMatcher =
+  `^(?!.*(${groupTestNames.map(names => names.join('|')).join('|')})).*$`;
+fs.writeFileSync('./test-groups/other.txt', otherTestsMatcher);
+
+// Write the results so we can use them for calculating averages in the next
+// build
+fs.writeFileSync("./test-groups/data.json", JSON.stringify(allBuildResults)); 
